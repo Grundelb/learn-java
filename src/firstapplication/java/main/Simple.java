98d67100dec090b7e8403333dbdfcd2149e3c2f1
Grigorii Shakhin <grigorii_shakhin@epam.com>
2022-09-05 21:54:46 +0200

    Add Project and first homework

---
+++ src/firstapplication/java/main/Simple.java
@@ -0,0 +1,8 @@
+package firstapplication.java.main;
+
+public class Simple {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+
+}
---
+++ src/learn/java/main/LearnMain.java
@@ -0,0 +1,12 @@
+package learn.java.main;
+
+public class LearnMain {
+    public static void main(String[] args) {
+        Double d = Double.POSITIVE_INFINITY;
+        double d1 = d.intValue() + Double.NEGATIVE_INFINITY;
+        System.out.println(d1);
+
+
+    }
+
+}
